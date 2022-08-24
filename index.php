013c18a3ca9fb9024df517fd8a7a4cfc76416db9
Carlo1891 <carlo.18@live.it>
2022-08-24 16:20:28 +0200

    style:

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FAQ Google</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     </head>
 
     <!-- Ci sono diverse domande con relative risposte.
@@ -13,18 +14,57 @@
     Come discusso a lezione, per parti ancora più composte e/o stilizzazioni particolari, sentitevi libere e liberi di utilizzare il markup all'interno delle stringhe nella struttura dati. -->
 
     <body>
-        <?php
-            include 'data.php';
-        ?>
+        <header>
+            <div class="top-bar d-flex">
 
-        <?php
+                <figure class="mx-2">Logo Google</figure>
+                <h4>Privacy e Termini</h4>
 
-        foreach ($faq as $value) {
-            echo '<h1>'. $value ['question'] . '</h1>';
-            echo '<p>'. $value ['answer'] . '</p>';
-        }
+            </div>
 
-        ?>
+            <nav>
+                <ul>
+                    <li>Introduzione</li>
+                    <li>Norme sulla privacy</li>
+                    <li>Termini di servizio</li>
+                    <li>Tecnologie</li>
+                    <li>Domande Frequenti</li>
+                </ul>
+            </nav>
+
+            <hr>
+        </header>
+        
+        <main>
+            <?php
+                include 'data.php';
+            ?>
+
+            <div class="container">
+                <?php
+
+                    foreach ($faq as $value) {
+                        echo '<h3>'. $value ['question'] . '</h3>';
+                        echo '<p>'. $value ['answer'] . '</p>';
+                    }
+
+                ?>
+            </div>
+        </main>
+        
+        <footer>
+            <div class="bottom-bar container">
+                <nav>
+
+                    <ul></ul>
+
+                </nav>
+                
+                <div>
+
+                </div>
+            </div>
+        </footer>
         
     </body>
 </html>

23993adcd69c3ebd8e8556fd03b1add304c3d0a5
Carlo1891 <carlo.18@live.it>
2022-08-24 15:22:11 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FAQ Google</title>
+    </head>
+
+    <!-- Ci sono diverse domande con relative risposte.
+    Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+    Note:
+    Come discusso a lezione, per parti ancora più composte e/o stilizzazioni particolari, sentitevi libere e liberi di utilizzare il markup all'interno delle stringhe nella struttura dati. -->
+
+    <body>
+        
+    </body>
+</html>
+
+
+
